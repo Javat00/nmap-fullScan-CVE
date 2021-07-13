19a55d7e1d6e839170d3718d12e8c9a98613362a
Aaron Espinosa Asencio <espinosa.asencio.aaron@gmail.com>
2021-07-13 19:16:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import nmap
+
+def full_scan():
+    ip = input("Host to scan: ")
+    nm = nmap.PortScanner()
+    nm.scan(ip, '1', '-v') #first we check if the host is up or not
+    if (nm[ip].state() == 'down'):
+        print("Host seems down")
+        exit()
+
+    else:
+        ports = []
+        #default is set to scan all ports 1-65535, you can change it if you want
+        nm.scan(ip, '1-65535', arguments='--open -T5 -n') #the higher the -T argument, the noisier and the faster it will be (from 0 to 5)
+        for host in nm.all_hosts():            
+            print('----------------------------------------------------')
+            print(f'Host : {ip}')
+            for proto in nm[host].all_protocols():
+                print('----------------------------------------------------')
+                print('Protocol : %s' % proto)
+                lport = nm[host][proto].keys()
+                for port in lport:
+                    ports.append(port)
+                    print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+        print("\n Applying some scripts and generating some logs, plase wait...")
+        open_ports = ','.join([str(item) for item in ports])
+        nm.scan(ip, open_ports, arguments='-sC -sV -oN target')
+        nm.scan(ip, open_ports, arguments=' --script vulscan/vulscan --script-args vulscandb=cve.csv -sV -oN target_cve')
+        print("\n Done! Two logs have been generated.")
+
+if __name__ == "__main__":
+    full_scan()
+    
+
+                
